f547919637be9e5019ea0d90053c1509afe5df42
Philip McNamara <philipmcnamara1@gmail.com>
2021-10-27 19:45:18 +0100

    fornt end bug fixes

--- app/src/main/java/org/wit/myassignment/activities/LoginActivity.kt
+++ app/src/main/java/org/wit/myassignment/activities/LoginActivity.kt
@@ -11,6 +11,8 @@ class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+
         btnRegLogin.setOnClickListener {
             startActivity(Intent(this,RegisterActivity::class.java))
             overridePendingTransition(R.anim.slide_from_right,R.anim.slide_to_left)
